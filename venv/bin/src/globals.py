f53986820ddcbf92185b71e8a3d447796aa9498d
Aniruddha - Kali PC <aniruddha2008@gmail.com>
2019-11-27 13:21:19 +0530

    log hashes to xlsx

--- venv/bin/src/globals.py
+++ venv/bin/src/globals.py
@@ -4,6 +4,7 @@ import os
 import analyze_code
 import analyze_apk
 import getopt
+import xlsxwriter
 
 #Global variables
 gv_path_to_base = None
@@ -27,7 +28,7 @@ def write_to_file(p_text_to_write, p_file_to_write):
 #**************************************************************************
 #Calculates MD5 of any given file
 #**************************************************************************
-def calculate_hash(p_file_to_identify, p_file_to_write):
+def calculate_hash(p_file_to_identify):
     #File pointer initialized in append mode
     try:
         lv_file_pointer = open(p_file_to_identify,"rb")
@@ -36,25 +37,41 @@ def calculate_hash(p_file_to_identify, p_file_to_write):
         # Create relative path
         lv_filename = os.path.relpath(p_file_to_identify, gv_path_to_base)
         #Write to file
-        write_to_file(lv_md5 + " " + lv_filename + "\n", p_file_to_write)
+        #write_to_file(lv_md5 + " " + lv_filename + "\n", p_file_to_write)
         #Closes the file
         lv_file_pointer.close()
+        return lv_md5 , lv_filename
     except:
         lv_filename = os.path.relpath(p_file_to_identify, gv_path_to_base)
         write_to_file("Error hashing file: " + lv_filename + "\n", gv_path_to_error_file)
+        return "Error hashing file", lv_filename
 
 #**************************************************************************
 #Calculates MD5 of source code and APK
 #**************************************************************************
 def identify_target():
-    write_to_file("START OF: SOURCE CODE Identification Info\n", "logs/log_v0.0.txt")
-    # Log all MD5 info of APK
-    write_to_file("\nResults for: MD5 and APK\n", "logs/log_v0.0.txt")
-    calculate_hash(gv_path_to_apk_file , "logs/log_v0.0.txt")
+    #write_to_file("START OF: Identification Info\n", "logs/log_v0.0.txt")
+    workbook = xlsxwriter.Workbook("logs/hash.xlsx")
+    worksheet = workbook.add_worksheet("html_checker")
+    #Header row of the excel sheet
+    excel_row = 0
+    worksheet.write(excel_row, 0, "MD5 Hash")
+    worksheet.write(excel_row, 1, "Artefact")
 
-    # Log all MD5 info of Source code files
-    write_to_file("\nResults for: MD5 and Source Code\n", "logs/log_v0.0.txt")
+    # Calculate MD5 info of APK
+    lv_hash, lv_path = calculate_hash(gv_path_to_apk_file)
+    excel_row += 1
+    worksheet.write(excel_row, 0, lv_hash)
+    worksheet.write(excel_row, 1, lv_path)
+
+    # Calculate MD5 info of Source code files
     for root, dirs, files in os.walk(gv_path_to_code_folder):
             for file in files:
-                  calculate_hash(os.path.join(root, file), "logs/log_v0.0.txt")
+                lv_hash, lv_path = calculate_hash(os.path.join(root, file))
+                excel_row += 1
+                worksheet.write(excel_row, 0, lv_hash)
+                worksheet.write(excel_row, 1, lv_path)
+
+    workbook.close()
     print("Completed V0.0 by: " + str( (datetime.datetime.now() - gv_time_start ).total_seconds() ) + " seconds")
+
